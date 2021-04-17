6959a316a1063f64d44001fe98651eacfa683d51
Zhanyong Wan <41527376+zhanyong-wan@users.noreply.github.com>
2021-04-17 15:46:47 -0700

    Add class Person.

--- source/family_tree.py
+++ source/family_tree.py
@@ -6,8 +6,27 @@ graphviz (dot).
 
 __author__ = 'Zhanyong Wan'
 
+from typing import Optional, Text
+
+def _GetDefaultIdFromName(name: Text) -> Text:
+  """Gets a person's default ID from their name."""
+
+  return name.replace(' ', '')
+
+class Person:
+  """Represents a person."""
+
+  def __init__(self, name: Text, id: Optional[Text]=None):
+    self.name = name
+    self.id = id if id else _GetDefaultIdFromName(name)
+
+
 class Family:
   """Represents a family of people."""
 
   def __init__(self):
-    pass
+    self.people = {}  # Maps ID to person.
+
+  def Add(self, person) -> 'Family':
+    self.people[person.ID()] = person
+    return self
